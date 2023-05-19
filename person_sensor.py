9cd22ec4aeb07a15dde3d711b435af5fef2b2725
Udinanon <m.trapanotto@tutamail.com>
2023-05-19 20:58:42 +0200

    Added PyGame to requirements PyGame used to show sensor's perception

--- person_sensor.py
+++ person_sensor.py
@@ -2,21 +2,43 @@
 import serial
 import json
 import time
+import pygame
 
 serialcomm = serial.Serial('/dev/ttyUSB0', 9600)
 serialcomm.timeout = 1
 
 def main():
+    # Setup Pygame
+    pygame.init()
+    screen = pygame.display.set_mode((255, 255))
+    clock = pygame.time.Clock()
+    running = True
+
     while True:
         recv_string = serialcomm.readline().decode('ascii')
         #print(recv_string)
         try:
             recv_data = json.loads(recv_string)
-            print("OK")
+            # print("OK")
             print(recv_data["n_faces"])
+             # fill the screen with a color to wipe away anything from last frame
+            screen.fill("black")
+            for face_index in range(recv_data["n_faces"]):
+                face = recv_data["faces"][face_index]
+                pos = pygame.Rect(tuple(face["box"]))
+                
+                pygame.draw.rect(screen, "white", pos)
+
+
         except:
             
             pass
+        # RENDER YOUR GAME HERE     
+
+        # flip() the display to put your work on screen
+        pygame.display.flip()
+
+        clock.tick(60)  # limits FPS to 60
     serialcomm.close()
     
 # Press the green button in the gutter to run the script.
